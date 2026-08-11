2e86437bbcee1c04941abc4c6352538de76dfef1
bhagyashridongaonkar06-dot <bhagyashridongaonkar06@gmail.com>
2026-08-11 19:35:21 +0530

    edit and update functionality added

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@ const lnameControl = document.getElementById('lname')
 const emailControl = document.getElementById('email')
 const contactControl = document.getElementById('contact')
 const stdContainer = document.getElementById('stdList')
+const addStd = document.getElementById('addStd')
+const updateStd = document.getElementById('updateStd')
 
 let stdArr = [
     {
@@ -64,7 +66,7 @@ function onCreateStdList(arr) {
     let res = '';
 
     arr.forEach((ele, i) => {
-        res += `<tr>
+        res += `<tr id="${ele.stdId}">
                     <td>${i + 1}</td>
                     <td>${ele.fname}</td>
                     <td>${ele.lname}</td>
@@ -80,7 +82,60 @@ function onCreateStdList(arr) {
 
 onCreateStdList(stdArr)
 
+function onEditStdList(ele){
+    //get edit id
+    let editId = ele.closest('tr').id;
+    //cl(editId)
 
+    //get edit object
+    let editObj = stdArr.find(a => a.stdId === editId)
+    //cl(editObj)
+
+    // patch value in data
+    fnameControl.value = editObj.fname
+    lnameControl.value = editObj.lname
+    emailControl.value = editObj.email
+    contactControl.value = editObj.contact
+
+    updateStd.setAttribute('data-edit-id', editId)
+
+    //show update btn and hide add btn
+    addStd.classList.add('d-none')
+    updateStd.classList.remove('d-none')
+}
+
+function onUpdate(){
+    let updateId = this.getAttribute('data-edit-id')
+    this.removeAttribute('data-edit-id')
+    //cl(updateId)
+
+    //get update Obj
+    let updateObj = {
+        fname : fnameControl.value,
+        lname : lnameControl.value,
+        email : emailControl.value,
+        contact : contactControl.value
+    }
+    cl(updateObj)
+
+    //we have to replace old object with updateObj
+    let getIndex = stdArr.findIndex(e => e.stdId === updateId)
+    stdArr[getIndex] = updateObj
+
+    let tr = document.getElementById(updateId).children; 
+    //cl(tr)
+    tr[1].innerHTML= updateObj.fname;
+    tr[2].innerHTML= updateObj.lname;
+    tr[3].innerHTML= updateObj.email; 
+    tr[4].innerHTML= updateObj.contact;
+
+
+    stdForm.reset()
+    //hide update btn and show add btn
+    updateStd.classList.add('d-none')
+    addStd.classList.remove('d-none')
+
+}
 
 
 function onStdAdd(eve) {
@@ -123,3 +178,4 @@ function onStdAdd(eve) {
 
 
 stdForm.addEventListener('submit', onStdAdd)
+updateStd.addEventListener('click', onUpdate)
